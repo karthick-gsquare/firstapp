76c4044a6a9fe54285d8da23c4fcc572d1801445
karthick-gsquare <karthick.v@gsquarehousing.com>
2025-03-20 15:32:16 +0530

    Tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 function App() {
   return (
     <>
-      <p>Hello world</p>
+      <div className="flex justify-center items-center min-h-dvh">
+        <center>
+          <h1 className="text-xl font-semibold bg-lime-200 inline-block p-2 rounded">
+            Hello world!
+          </h1>
+          <p>Hello world</p>
+        </center>
+      </div>
     </>
   );
 }
